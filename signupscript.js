143444d9adab9ab1235f01ce54f84342fa3e7e01
saikiran12040 <saikirangen@gmail.com>
2022-08-25 21:44:27 +0530

    added file

---
+++ signupscript.js
@@ -0,0 +1,18 @@
+document.querySelector("#signup").addEventListener("submit",createfunc);
+    let Arr=JSON.parse(localStorage.getItem("storeDetails")) || [];
+    function createfunc(){
+        event.preventDefault();
+        obj={
+            email:document.querySelector("#email").value,
+            password:document.querySelector("#password").value,
+        }
+        if (obj.email=="" || obj.password==""){
+            alert("Invalid input")
+            return;
+        } else {
+            alert("signed up successfully")
+            window.location.href="login.html"
+        }
+        Arr.push(obj);
+        localStorage.setItem("storeDetails",JSON.stringify(Arr));
+    }
